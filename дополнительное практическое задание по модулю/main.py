7c19a6c6b7ca815177813297a7d326c70765694c
Proscka <proskinaviktoria861@gmail.com>
2024-07-08 18:01:46 +0300

    Add files via upload

---
+++ дополнительное практическое задание по модулю/main.py
@@ -0,0 +1,24 @@
+journal={"Aaron":[5,3,5,4],"Bilbo":[2,2,2,3],"Johnny":[4,5,5,2],
+           "Khendrik":[4,4,3],"Steve":[5,5,4,5]}
+print(journal)
+print(tuple)
+journal={"Aaron":[5,3,5,4]}
+print(sum([5,3,5,4]))
+print(17/4)
+journal={"Bilbo":[2,2,2,3]}
+print(sum([2,2,2,3]))
+print(9/4)
+journal={"Johnny":[4,5,5,2]}
+print(sum([4,5,5,2]))
+print(16/4)
+journal={"Khendrik":[4,4,3]}
+print(sum([4,4,3]))
+print(11/3)
+journal={"Steve":[5,5,4,5]}
+print(sum([5,5,4,5]))
+print(19/4)
+journal={"Aaron":4,"Bilbo":2,"Johnni":4,"Khendrik":3,"Steve":4}
+print(journal)
+
+
+
